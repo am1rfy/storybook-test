debe9a2d2f80a9015c8400c237d114476ae69735
am1rfy <fajzov.amir.gl@gmail.com>
2023-08-28 02:34:50 +0500

    sass added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            sass: {
+                additionalData: `
+                    @import './src/assets/css/variables.sass'
+                ` 
+            },
+        },
+    },
 })
